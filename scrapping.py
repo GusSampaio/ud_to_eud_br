36dd502bf12fa188f4a24cb31df6a7dc3fd56501
GusSampaio <gustavo.sampaio@usp.br>
2024-05-16 22:29:14 -0300

    Adicionando classificacao de regras para cada aplicação

--- scrapping.py
+++ scrapping.py
@@ -8,13 +8,19 @@ import csv
 
 def cria_csv():
     #Criando arquivo csv para análise futura
-    with open('aplicacoes.csv', 'w', newline='') as arquivo:
+    with open('aplicacoes.csv', 'w', newline='', encoding='utf-8') as arquivo:
         writer = csv.writer(arquivo)
-        writer.writerow(['id_sentenca','pacote', 'regra'])
+        writer.writerow(['id_sentenca', 'tipo_pacote', 'pacote', 'regra'])
+
+def get_key(dict, pack):
+    for key, value in dict.items():
+        if pack in value:
+            return key
+    return "Sem Classificacao"
 
 #Insercao das regras no csv
-def regras_no_csv(num_items, driver, id_name):
-    with open('aplicacoes_teste.csv', 'a', newline='') as arquivo:
+def regras_no_csv(num_items, driver, id_name, classificacao_regras):
+    with open('aplicacoes.csv', 'a', newline='', encoding='utf-8') as arquivo:
         writer = csv.writer(arquivo)
         for i in range(num_items):
             applied_rules_btn = WebDriverWait(driver, 10).until(
@@ -24,7 +30,10 @@ def regras_no_csv(num_items, driver, id_name):
             rule = rule.split(' ')[2]
             rule = rule.split('.')
             rule.insert(0, id_name)
+            classificacao = get_key(classificacao_regras, rule[1])
+            rule.insert(1, classificacao)
             writer.writerow(rule)
+
 
 def scrapp():
     driver = webdriver.Firefox()
@@ -34,6 +43,16 @@ def scrapp():
     #Requisicao da página
     driver.get(url)
 
+    classificacao_regras = {
+        'Acréscimo de preposições e conjunções': ['case_info', 'cc_info'],
+        'Propagação de sujeitos de xcomp': ['deep_subj_ud'],
+        'Propagação de head compartilhado por elementos coordenados': ['coord_gov'],
+        'Propagação de dependentes de elementos coordenados': ['coord_dep_left_pck', 'coord_dep_right_pck'],
+        'Anotação do referente dos pronomes relativos': ['rel_pron'],
+        'Inclusão do predicado elíptico': ['null_nodes_head', 'null_nodes_dep', 'orphan_default_pck'],
+        'Melhorias': ['elvis', 'elvis_orphan']
+        }
+    
     try:
         # Esperando que os elementos fiquem disponiveis para scrapping
         input_corpus = WebDriverWait(driver, 10).until(
@@ -44,7 +63,7 @@ def scrapp():
         sleep(3)
 
         #Inserção do corpus
-        input_corpus.send_keys(os.getcwd()+"\Pre_Anotacao_Enhanced.conllu")
+        input_corpus.send_keys(os.getcwd()+"\sentencas_teste.conllu")
 
         #Inserção das regras
         input_regras = driver.find_element(By.ID, 'grs_file_input').send_keys(os.getcwd()+"\conjunto_regras_oficial.grs")
@@ -57,7 +76,8 @@ def scrapp():
         
         cria_csv()
 
-        strategie_name = 'meu_teste'
+        strategie_name = 'strat_principal'
+
         if num_sentences == 1:     
             #Aplicação das regras
             strategies_btn = WebDriverWait(driver, 10).until(
@@ -80,7 +100,7 @@ def scrapp():
 
             num_items = len(applied_rules)
 
-            regras_no_csv(num_items, driver, id_name)
+            regras_no_csv(num_items, driver, id_name, classificacao_regras)
         else:
             for i in range(num_sentences):
                 changed_sentences[i].click()
@@ -97,13 +117,11 @@ def scrapp():
                 see_rules_btn.click()
 
                 sentences_elements = driver.find_elements(By.XPATH, '//div[@class="col-md-9 ml-sm-auto col-lg-10 px-md-4"]/h3')
+
                 sleep(0.5)
 
-                # Primeira sentenca considera o id do documento
-                if i == 0:
-                    full_text = sentences_elements[9].text
-                else:
-                    full_text = sentences_elements[6].text
+                
+                full_text = sentences_elements[6].text
 
                 id_name = full_text.split(': ')[1]
 
@@ -113,7 +131,7 @@ def scrapp():
 
                 num_items = len(applied_rules)
 
-                regras_no_csv(num_items, driver, id_name)
+                regras_no_csv(num_items, driver, id_name, classificacao_regras)
                 
                 sleep(1)
 
